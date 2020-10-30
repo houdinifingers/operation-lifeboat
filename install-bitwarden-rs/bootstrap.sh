3e37e3af7f3ce05d1e8d1c61db810df5e221e524
houdinifingers <houdinifingers@gmail.com>
2020-10-30 03:35:19 -0400

    actually commited helm install command for bootstrap script, renamed abandonship.sh to abandonship.txt to be used for documenting how to use this project.

---
+++ install-bitwarden-rs/bootstrap.sh
@@ -0,0 +1,4 @@
+# This file is used for helm installs of various lifeboat services
+
+# use --set foo=bar multiple times to set kv pairs
+helm install -f install-bitwarden-rs/values.yaml install-bitwarden-rs
