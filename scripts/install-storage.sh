e65a692dc1b79548bceb0bbaacc92ebd4a3c469b
unaHm <66929375+unaHm@users.noreply.github.com>
2020-10-30 14:42:46 -0400

    Update install-storage.sh

--- scripts/install-storage.sh
+++ scripts/install-storage.sh
@@ -1,42 +1,39 @@
 #!/bin/bash
 
+# Inspired by: https://rancher.com/docs/k3s/latest/en/storage/
+# Quote:
+# K3s comes with Rancher’s Local Path Provisioner and
+# this enables the ability to create persistent volume claims
+# out of the box using local storage on the respective node.
+
 # Make a local directory to mount to:
 
 mkdir /k3s-storage
 
-# Create the PersistentVolumeClaim and the StorageClass pod in Kubernetes
+echo "In order to use the local path provisioner, please set the storageClass name as 'local-path' when deploying helm charts."
+echo "Before deploying a helm chart that you want to include persistent storage, "
+echo" please create a directory within /k3s-storage for that application,"
+echo "and ensure your volume mounts point to that directory."
 
-cat <<EOF | kubectl apply -f -
-apiVersion: v1
-kind: PersistentVolumeClaim
-metadata:
-  name: local-path-pvc
-  namespace: default
-spec:
-  accessModes:
-    - ReadWriteOnce
-  storageClassName: local-path
-  resources:
-    requests:
-      storage: 10Gi
----
-apiVersion: v1
-kind: Pod
-metadata:
-  name: volume-test
-  namespace: default
-spec:
-  containers:
-  - name: volume-test
-    image: nginx:stable-alpine
-    imagePullPolicy: IfNotPresent
-    volumeMounts:
-    - name: volv
-      mountPath: /k3s-storage
-    ports:
-    - containerPort: 80
-  volumes:
-  - name: volv
-    persistentVolumeClaim:
-      claimName: local-path-pvc
-EOF
+# Create the PersistentVolumeClaim in Kubernetes
+
+#cat <<EOF | kubectl apply -f -
+#apiVersion: v1
+#kind: PersistentVolumeClaim
+#metadata:
+#  name: local-path-pvc
+#  namespace: default
+#spec:
+#  accessModes:
+#    - ReadWriteOnce
+#  storageClassName: local-path
+#  resources:
+#    requests:
+#      storage: 10Gi
+#EOF
+
+# To use this in further deployments, refer to these values:
+#  volumes:
+#  - name: <NAME>
+#    persistentVolumeClaim:
+#      claimName: local-path-pvc
