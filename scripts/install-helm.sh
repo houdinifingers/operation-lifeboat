afd18a472ca37294c36d26263b732490de49aa60
unaHm <66929375+unaHm@users.noreply.github.com>
2020-10-30 13:04:51 -0400

    Update install-helm.sh

--- scripts/install-helm.sh
+++ scripts/install-helm.sh
@@ -1,10 +1,10 @@
+#!/bin/bash
 # Install latest stable version of helm 3
+# Attribution
+# https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 
-$ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-$ chmod 700 get_helm.sh
-$ ./get_helm.sh
-# ATTRIBUTION #
-# https://helm.sh/docs/intro/install/
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
 
-# Remove helm setup script
-$ rm -f get_helm.sh
+# Run the install script, and remove if complete
+./get_helm.sh && rm -f get_helm.sh
