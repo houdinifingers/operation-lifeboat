fe0238652e79d35b1eb0496dee013604b65096ed
unaHm <66929375+unaHm@users.noreply.github.com>
2020-10-30 14:17:32 -0400

    Update install-freeipa.sh

--- scripts/install-freeipa.sh
+++ scripts/install-freeipa.sh
@@ -7,3 +7,11 @@ helm repo add cocainefarm https://kube.cat/chartrepo/cocainefarm
 # Install the helm chart
 
 helm install my-freeipa-self-service cocainefarm/freeipa-self-service --version 1.0.0
+
+# TODO: Determine if we need to store data persistently, with commands such as:
+# --set persistence.enabled=true
+# --set persistence.size=1Gi
+# Determine ingress, with commands such as:
+# --set ingress.enabled=true
+# --set ingress.host=freeipa.mylocaldomain
+# and whether we need any HTTPs certificates
