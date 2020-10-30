70fb89d9433738bc8d160abd3fa5447e53117bc6
unaHm <66929375+unaHm@users.noreply.github.com>
2020-10-30 12:57:21 -0400

    Update sos.sh scrape latest version from github

--- scripts/sos.sh
+++ scripts/sos.sh
@@ -1,8 +1,11 @@
-#!/bin/#!/usr/bin/env bash
+#!/bin/bash
 
 # This script installs helm
 
+version=$(curl https://github.com/helm/helm/releases/latest | awk -F '/' '{print $8}' | cut -d "\"" -f 1)
+
 cd /tmp
-wget https://get.helm.sh/helm-v3.4.0-linux-amd64.tar.gz
-tar -zxvf helm-v3.4.0-linux-amd64.tar.gz
+
+wget "https://get.helm.sh/helm-${version}-linux-amd64.tar.gz" -O helm.tar.gz
+tar -zxvf helm.tar.gz
 mv linux-amd64/helm /usr/local/bin/helm
