ced5bc73fc2e3397b69e928c2e84ba4d7cd1240e
houdinifingers <houdinifingers@gmail.com>
2020-10-30 05:46:04 -0400

    updated readme and created user guide. updated bootstrap script

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 A small footprint K8s lifeboat infrastructure focused on restoring services after a cyber attack.
 
 See abandonship.txt for detailed setup instructions.
+See gilligan.txt for detailed user instructions.
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 # This file is used for helm installs of various lifeboat services
+# This script expects only one parameter --domain-name by default
 
 # use --set foo=bar multiple times to set kv pairs
 helm install -f install-bitwarden-rs/values.yaml install-bitwarden-rs
