7a01bc35dc1e679952462b3d150c126104a85146
houdinifingers <houdinifingers@google.com>
2020-10-31 01:44:18 -0400

    Created helm install script for bitwarden

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
-# This file is used for helm installs of various lifeboat services
-# This script expects only one parameter --domain-name by default
 
-# use --set foo=bar multiple times to set kv pairs
+# Bootstrap script to install all applications.
 
-# install free IPA
-
-# install bitwarden
-
-helm install -f install-bitwarden-rs/values.yaml install-bitwarden-rs
-
-
+# Install bitwarden
+scripts/helm-install-bitwarden-rs.sh
